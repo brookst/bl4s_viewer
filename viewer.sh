09f0a0cca1b8c65474cc2531e7b770dff146a0da
Tim Brooks <brooks@cern.ch>
2015-02-21 12:15:09 +0100

    Download the image if needed before running

--- viewer.sh
+++ viewer.sh
@@ -2,5 +2,10 @@
 #
 #Tim Brooks 2015 <brooks@cern.ch>
 
+IMAGE=brookst/bl4s_viewer
+
+#If needed, fetch the image from the Docker registry
+docker images | grep "$IMAGE" || docker pull "$IMAGE"
+
 #Run bl4s_viewer image connected to the host X server
 docker run -ti --rm -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix brookst/bl4s_viewer
